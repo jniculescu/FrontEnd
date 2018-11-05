4676a8ed9e9bb4806373b44177b3189bab15cca6
jniculescu <jani.niculescu@gmail.com>
2018-11-05 11:04:18 +0200

    progoress

--- KLI1317-contacts-app/src/app/app.component.ts
+++ KLI1317-contacts-app/src/app/app.component.ts
@@ -6,23 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'KLI1317-contacts-app';
-  name: string;
-  greeting: string;
-  greetings: string[];
+
 
   constructor()
   {
-    this.greeting = 'hello, im bored';
-    this.name = ' oh, my...god';
-    this.greetings = [];
-  }
 
-  onSubmitGreeting()
-  {
-    let submittedGreeting: string;
-    //const  submittedGreeting = 'hello, ' + this.name + '.';
-    this.greetings.push(submittedGreeting);
-    console.log(this.greetings);
   }
 }
--- KLI1317-contacts-app/src/app/app.module.ts
+++ KLI1317-contacts-app/src/app/app.module.ts
@@ -2,17 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import {FormsModule} from '@angular/forms';
+import { ContactListComponent } from './contact/contact-list/contact-list.component';
+import { ContactListItemComponent } from './contact/contact-list-item/contact-list-item.component';
+import {ContactService} from './contact/services/contact.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactListComponent,
+    ContactListItemComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
   ],
-  providers: [],
+  providers: [ContactService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
