3c70ecd499f79fe3c71e29f5f78a1b73f914c656
jniculescu <jani.niculescu@gmail.com>
2018-11-05 08:46:44 +0200

    .

--- KLI1317-contacts-app/src/app/app.component.ts
+++ KLI1317-contacts-app/src/app/app.component.ts
@@ -7,4 +7,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'KLI1317-contacts-app';
+  name: string;
+  greeting: string;
+  greetings: string[];
+
+  constructor()
+  {
+    this.greeting = 'hello, im bored';
+    this.name = ' oh, my...god';
+    this.greetings = [];
+  }
+
+  onSubmitGreeting()
+  {
+    let submittedGreeting: string;
+    //const  submittedGreeting = 'hello, ' + this.name + '.';
+    this.greetings.push(submittedGreeting);
+    console.log(this.greetings);
+  }
 }
